6dfa42a2635ce341122848fbad08e29613d01a42
kridi32 <kridi32@outlook.com>
2022-07-25 22:37:44 +0600

    left part is done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import logo from "./Img/Logo.png"
+import { ImQuotesLeft } from 'react-icons/im'
+import { GoVerified } from 'react-icons/go'
+import angle from './Img/Angle.png'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App min-h-screen min-w-screen  flex justify-center items-center">
+      <section className="w-full grid grid-cols-2">
+        <div className="w-full min-h-screen bg-img bg-cover relative">
+          <div className="absolute top-0 left-0 w-full h-full bg-[#1565D8] bg-opacity-90 opacity-100 p-12">
+            <div className="h-full">
+              <h2 className="text-2xl font-bold text-white text-left flex justify-items-start gap-2 mb-4">    <img src={logo} alt="" /> Oasis</h2>
+              <div className="min-h-full flex flex-col justify-center">
+                <h2 className="text-[#00DAF7] text-4xl"><ImQuotesLeft/></h2>
+                <p className="text-white text-left ">The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software.</p>
+                <h5 className="text-lg text-white my-4 font-bold text-left flex  items-center gap-2">Vincent Obi  <GoVerified className="text-green-600 bg-white rounded-full" /></h5>
+                <div className="w-full flex justify-end"><img src={angle} alt="" /></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
