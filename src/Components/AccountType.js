ae4fb29f57cca732b18a1c9800fe05c693c72a46
kridi32 <kridi32@outlook.com>
2022-07-26 09:02:59 +0600

    first page is done

---
+++ src/Components/AccountType.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+const AccountType = ({lineImg, filledImg, type, message}) => {
+    const [hover, setHover] = useState(true);
+  return (
+    <Link to="/basic"
+    className="w-full bg-white p-4 rounded-lg shadow-md flex items-center gap-2 hover:bg-[#F5F9FF] hover:text-[#1565D8] my-2 border border-white hover:border-[#1565D8]"
+    onMouseEnter={(e) => setHover(false)}
+    onMouseLeave={(e) => setHover(true)}
+  >
+    {hover ? (
+      <img src={lineImg} alt="" />
+    ) : (
+      <img src={filledImg} alt=""/>
+    )}
+            <div className="p-2 ">
+            <h3 className="text-xl  text-left font-semibold ">
+      {type}
+            </h3>
+              <p className="text-left text-[#8692A6] leading-4 mt-1">{message}</p>
+   </div>
+  </Link>
+  )
+}
+
+export default AccountType
--- src/Components/AuthHome.js
+++ src/Components/AuthHome.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import personalOutlined from "../Img/personal-outline.png";
 import personalFilled from "../Img/Account-filled.png";
+import personalOutlined from "../Img/personal-outline.png";
+import AccountType from "./AccountType";
+import businessFilled from "../Img/businee-filed.png"
+import businessOutlined from '../Img/business-outlined.png'
 const AuthHome = () => {
-  const [hover, setHover] = useState(true);
+ 
   return (
     <div className="p-12 ">
       <p className="text-right text-[#8692A6] ">
@@ -17,23 +20,8 @@ const AuthHome = () => {
         <p className="text-left text-[#8692A6] mt-2">
           To begin this journey, tell us what type of account you’d be opening.
         </p>
-        <Link to="/"
-          className="w-full bg-white p-4 rounded-lg shadow-md flex items-center gap-2 hover:bg-[#F5F9FF] hover:text-[#1565D8] my-2 hover:border hover:border-[#1565D8]"
-          onMouseEnter={(e) => setHover(false)}
-          onMouseLeave={(e) => setHover(true)}
-        >
-          {hover ? (
-            <img src={personalOutlined} alt="" />
-          ) : (
-            <img src={personalFilled} />
-          )}
-                  <div className="p-2 ">
-                  <h3 className="text-xl  text-left font-semibold ">
-            Individual
-                  </h3>
-                  <p className="text-left text-[#8692A6] leading-4 mt-1">Personal account to manage all you activities.</p>
-         </div>
-        </Link>
+        <AccountType lineImg={personalOutlined} filledImg={personalFilled} type="Individual" message="Personal account to manage all you activities." />
+        <AccountType lineImg={businessOutlined} filledImg={businessFilled} type="Business" message="Own or belong to a company, this is for you." />
       </div>
     </div>
   );
